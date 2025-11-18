e83eaa230470958ff0e99380f0b486ef971c2669
Nathaniel Escade <nathanielescade@gmail.com>
2025-11-18 09:45:09 +0000

    added verfify email component in a suspense

--- src/app/verify-email/page.js
+++ src/app/verify-email/page.js
@@ -1,7 +1,12 @@
 "use client";
 
-import VerifyEmailClient from './VerifyEmailClient';
+import { Suspense } from "react";
+import VerifyEmailClient from "./VerifyEmailClient";
 
 export default function Page() {
-  return <VerifyEmailClient />;
+  return (
+    <Suspense fallback={<div className="text-white">Loading...</div>}>
+      <VerifyEmailClient />
+    </Suspense>
+  );
 }
