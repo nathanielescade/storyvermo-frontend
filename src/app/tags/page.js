74d572122905c86b2140c1d407b19094ca31e32a
Nathaniel Escade <nathanielescade@gmail.com>
2025-12-20 19:27:15 +0000

    removed /tags page

--- src/app/tags/page.js
+++
@@ -1,9 +0,0 @@
-// src/app/tags/page.js
-'use client';
-
-import dynamic from 'next/dynamic';
-const TagsClient = dynamic(() => import('../../components/TagsClient'), { ssr: false });
-
-export default function TagsPage() { 
-  return <TagsClient />; 
-} 
